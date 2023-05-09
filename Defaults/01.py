d154105156a0775a5f6ddb2220bef9d0dc1916c3
성현 <128461225+shyexnnn@users.noreply.github.com>
2023-05-10 01:29:44 +0900

    01.py

---
+++ Defaults/01.py
@@ -0,0 +1,21 @@
+# 문자열
+
+print("Hello World")
+
+multiline = '''
+Hello
+World
+!!!
+'''
+print(multiline) # (""")이나 (''')을 연속해서 쓰면 문자열을 여러줄 출력 가능
+
+head = "Hello"
+tail = "World"
+head + tail # 문자열 더해서 연결 가능
+
+a = "Hello World"
+a[1] # 문자열 인덱싱: 문자열 안의 특정 값 뽑아냄
+a = "Hello World"
+a[0:2] # 문자열 슬라이싱: a[시작번호:끝번호] 나타낸 자리만큼 뽑아냄
+
+
